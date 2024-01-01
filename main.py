476dfbdb7c0e2bd6a21c513952748ca80244d999
pett0 <llol4990@gmail.com>
2024-01-01 23:44:33 +0100

    letter count function + refactoring
    added vars.py file, refactored main.py code and added letter count function

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-PATH = "books/frankenstein.txt"
+from vars import PATH, alphabet_map
 
-with open(PATH) as f:
-    contents = f.read()
-    word_count = len(contents.split(" "))
-print(word_count)
+def word_count(contents):
+    return len(contents.split())
+
+def read_file(path):
+    with open(path) as f:
+        return f.read() 
+
+def letter_count(contents: str):
+    copy = alphabet_map.copy()
+    for c in list(contents.lower()):
+        if c.isalpha():
+            copy[c] += 1
+    return copy
+
+def main():
+    contents = read_file(PATH)
+    print(word_count(contents))
+    print(letter_count(contents))
+
+if __name__ == "__main__":
+    main()
+    
