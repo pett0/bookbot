cc508674f839d48365b9fda22abd1c84920f6c82
pett0 <llol4990@gmail.com>
2024-01-02 00:07:36 +0100

    pretty print
    added pretty print for symbols dictionraies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from vars import PATH, alphabet_map
+from vars import PATH
 
 def word_count(contents):
     return len(contents.split())
@@ -7,18 +7,32 @@ def read_file(path):
     with open(path) as f:
         return f.read() 
 
-def letter_count(contents: str):
-    copy = alphabet_map.copy()
-    for c in list(contents.lower()):
+def symbol_count(contents: str):
+    char_map = {}
+    for c in contents:
+        low = c.lower()
+        if low in char_map:
+            char_map[low] += 1
+        else:
+            char_map[low] = 1
+    return char_map
+
+def print_symbols(char_map):
+    print("Chars: ")
+    chars = list(char_map)
+    chars.sort()
+    for c in chars:
         if c.isalpha():
-            copy[c] += 1
-    return copy
+            print(f"The '{c}' appeared in book {char_map[c]} times.") 
+    print("-------------------------------------")
 
 def main():
     contents = read_file(PATH)
+    symbols = symbol_count(contents)
+
     print(word_count(contents))
-    print(letter_count(contents))
+    print_symbols(symbols)
 
 if __name__ == "__main__":
     main()
-    
+
